9ccda860227617280d3bee8d12aece67917a9738
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 03:50:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
-import PyPDF2
 import openai
 import streamlit as st
 
-# Solicita al usuario su clave API de OpenAI
-api_key = st.text_input("Ingresa tu clave API de OpenAI:")
+# Prompt the user for their API key
+api_key = st.text_input("Ingrese su clave API de OpenAI:")
 
-# Configura la API de OpenAI con la clave API proporcionada
+# Set up the OpenAI API with the provided API key
 openai.api_key = api_key
 
-# Define una función para extraer texto de un archivo PDF
+# Define a function to extract text from a PDF file
 def extract_text_from_pdf(file):
     pdf_reader = PyPDF2.PdfReader(file)
     num_pages = len(pdf_reader.pages)
@@ -18,56 +17,61 @@ def extract_text_from_pdf(file):
         text += page.extract_text()
     return text
 
-# Define una función para generar respuestas a preguntas de usuario utilizando la API de ChatGPT
-def generate_answer(question, text):
-    max_context_length = 4096 - len(question) - 30
-    truncated_text = text[:max_context_length]
-    prompt = f"{truncated_text}\n\nPregunta: {question}\nRespuesta:"
-    response = openai.Completion.create(
+# Define a function to evaluate the quality of an argument
+def evaluar_argumento(argumento):
+    # Realizamos una evaluación de la coherencia, la claridad, la relevancia y la validez lógica del argumento
+    # y devolvemos un puntaje en función de su calidad global.
+    # Esto es solo una implementación básica, y se podría adaptar según las necesidades específicas.
+
+    # Evaluación de la coherencia
+    coherencia_score = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=prompt,
+        prompt=f"Evaluación de la coherencia del argumento:\n\n{argumento}\n",
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.7,
-    )
-    answer = response.choices[0].text.strip()
-    return answer
+        temperature=0.5,
+    ).choices[0].text.strip()
 
-# Define una función para evaluar la comprensión del usuario basándose en el texto proporcionado
-def evaluate(text):
-    num_questions = 0
-    num_correct = 0
-    total_score = 0
-    questions = [
-        "¿Cuál es el tema principal del texto?",
-        "¿Cuál es la tesis del autor?",
-        "¿Cuáles son los argumentos que utiliza el autor para sostener su tesis?",
-        "¿Cuáles son las conclusiones del autor?",
-        "¿Cuál es tu opinión sobre el texto?",
-    ]
-    for question in questions:
-        user_answer = st.text_input(question)
-        if user_answer:
-            num_questions += 1
-            answer = generate_answer(question, text)
-            if answer.lower() == user_answer.lower():
-                num_correct += 1
-                total_score += 2
-            elif user_answer.lower() in answer.lower():
-                total_score += 1
-    if num_questions == 0:
-        return "Por favor, responde al menos una pregunta para evaluar tu comprensión."
-    else:
-        score = round((total_score / (num_questions * 2)) * 10, 2)
-        return f"Tu calificación es: {score}/10"
+    # Evaluación de la claridad
+    claridad_score = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Evaluación de la claridad del argumento:\n\n{argumento}\n",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).choices[0].text.strip()
 
-# Define una función para manejar la carga de archivos y la generación de respuestas
+    # Evaluación de la relevancia
+    relevancia_score = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Evaluación de la relevancia del argumento:\n\n{argumento}\n",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).choices[0].text.strip()
+
+    # Evaluación de la validez lógica
+    logica_score = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Evaluación de la validez lógica del argumento:\n\n{argumento}\n",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).choices[0].text.strip()
+
+    # Devolvemos un puntaje promedio para el argumento
+    puntaje = (float(coherencia_score) + float(claridad_score) + float(relevancia_score) + float(logica_score)) / 4.0
+    return puntaje
+
+# Define a function to handle the file upload and argument evaluation
 def handle_file_upload():
-    file = st.file_uploader("Sube un archivo PDF", type=["pdf"])
+    file = st.file_uploader("Cargar un archivo PDF", type=["pdf"])
     if file is not None:
         text = extract_text_from_pdf(file)
-        st.write(evaluate(text))
-
-# Llama a la función handle_file_upload para ejecutar la aplicación
-handle_file_upload()
+        argumento = st.text_area("Ingrese el argumento a evaluar:")
+        if st.button("Evaluar"):
+            puntaje
