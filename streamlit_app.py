30545848e4592dcb97294e6a473a7c615252e77c
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 05:10:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import PyPDF2
 import openai
 import streamlit as st
 
-
+# Solicita al usuario que ingrese su clave API de OpenAI
 api_key = st.text_input("Enter your OpenAI API key:")
 openai.api_key = api_key
 
+# Función para extraer texto de un archivo PDF
 def extract_text_from_pdf(file):
     pdf_reader = PyPDF2.PdfReader(file)
     num_pages = len(pdf_reader.pages)
@@ -15,6 +16,7 @@ def extract_text_from_pdf(file):
         text += page.extract_text()
     return text
 
+# Función para evaluar la calidad argumentativa del texto utilizando OpenAI
 def evaluate_argumentative_quality(text):
     prompt = f"Please evaluate the argumentative quality of the following text:\n\n{text}\n\nQuality:"
     response = openai.Completion.create(
@@ -28,6 +30,7 @@ def evaluate_argumentative_quality(text):
     quality = response.choices[0].text.strip()
     return quality
 
+# Función para manejar la carga de archivos y la evaluación
 def handle_file_upload():
     file = st.file_uploader("Upload a PDF file", type=["pdf"])
     if file is not None:
@@ -36,6 +39,7 @@ def handle_file_upload():
             quality = evaluate_argumentative_quality(text)
             st.write(f"Argumentative Quality: {quality}")
 
+# Función principal para ejecutar la aplicación Streamlit
 def main():
     handle_file_upload()
 
