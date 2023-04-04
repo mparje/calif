85dd771a41f82a783b583ee0bdb34c0b3761829b
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 13:24:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def extract_text_from_pdf(file):
 # Función para evaluar la calidad argumentativa del texto utilizando OpenAI
 def evaluate_argumentative_quality(text):
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="gpt-3.5-turbo",
         prompt=(
             f"Evalúa la calidad argumentativa de este texto:\n"
             f"{text}\n"
