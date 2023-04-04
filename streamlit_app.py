f398d2ca823bcf30fb8e8f0ca118cfe6aad7e8f5
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 04:39:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,10 @@ import PyPDF2
 import openai
 import streamlit as st
 
-# Prompt the user for their API key
-api_key = st.text_input("Enter your OpenAI API key:")
 
-# Set up the OpenAI API with the provided API key
+api_key = st.text_input("Enter your OpenAI API key:")
 openai.api_key = api_key
 
-# Define a function to extract text from a PDF file
 def extract_text_from_pdf(file):
     pdf_reader = PyPDF2.PdfReader(file)
     num_pages = len(pdf_reader.pages)
@@ -18,7 +15,6 @@ def extract_text_from_pdf(file):
         text += page.extract_text()
     return text
 
-# Define a function to evaluate the argumentative quality of the text
 def evaluate_argumentative_quality(text):
     prompt = f"Please evaluate the argumentative quality of the following text:\n\n{text}\n\nQuality:"
     response = openai.Completion.create(
@@ -32,17 +28,15 @@ def evaluate_argumentative_quality(text):
     quality = response.choices[0].text.strip()
     return quality
 
-# Define a function to handle the file upload and answer generation
+python
 def handle_file_upload():
     file = st.file_uploader("Upload a PDF file", type=["pdf"])
     if file is not None:
         text = extract_text_from_pdf(file)
-
         if st.button("Evaluate"):
             quality = evaluate_argumentative_quality(text)
             st.write(f"Argumentative Quality: {quality}")
 
-# Define a main function to run the program
 def main():
     handle_file_upload()
 
